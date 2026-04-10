7ac46e7bdf9c43ad27902dd1cdf3227031ad5e26
TahaNauman <mtahanauman90@gmail.com>
2026-04-10 14:26:34 +0500

    feat : add referrer header

--- web_scraper_suite/static_scraper/fetcher.py
+++ web_scraper_suite/static_scraper/fetcher.py
@@ -4,7 +4,7 @@ from fake_useragent import UserAgent
 import random
 import time
 
-def fetch_page(url, user_agent="MyScraper"):
+def fetch_page(url, user_agent="MyScraper",referer=None):
     # Check robots.txt
     rp = RobotFileParser()
     rp.set_url(url.rstrip("/") + "/robots.txt")
@@ -19,7 +19,15 @@ def fetch_page(url, user_agent="MyScraper"):
     except:
         ua = "Mozilla/5.0"
 
-    headers = {"User-Agent": ua}
+    headers = {
+        "User-Agent": ua,
+        "Accept-Language": "en-US,en;q=0.9",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    }
+
+    # Add the Referer logic
+    if referer:
+        headers["Referer"] = referer
 
     # Polite delay
     time.sleep(random.uniform(1,3))
